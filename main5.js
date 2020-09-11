fdec5a11e10d73c9d9ae2ac19b04bd6c8d69a846
Md.Asif Ur Rahman <45636891+asifurrahman01714@users.noreply.github.com>
2020-09-11 23:20:04 +0600

    Add files via upload

---
+++ main5.js
@@ -0,0 +1,13 @@
+
+var num1=parseInt(prompt("enter the first number: ") ) ;
+var num2=parseInt(prompt("enter the second number: ") ) ;
+var num3=parseInt(prompt("enter the third number: ") ) ;
+
+ if (num1> num2 && num1 >num3)
+ document.write("The Greater Number is : " + num1);
+ 
+ else if (num2> num1 && num2 >num3)
+ document.write("The Greater Number is : " + num2);
+
+ else
+ document.write("The Greater Number is : " + num3);
